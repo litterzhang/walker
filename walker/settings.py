4e0d4a436d45b1fe33ddadd20077e9814306cb2a
bitorange@foxmail.com <bitorange@foxmail.com>
2016-04-12 11:04:39 +0800

    我又将sqlite改为mysql了

--- walker/settings.py
+++ walker/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'walker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'climb',
-#         'USER':'root',
-#         'PASSWORD':'',
-#         'HOST':'',
-#         'PORT':'',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'climb',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':'',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
